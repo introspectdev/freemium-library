a3722ba97c7188a2c88ac56a6d4e77fed18984fd
Jason Burgess <introspect.dev@gmail.com>
2020-06-03 04:22:21 -0500

    Working version of the library. Still needs cleanup, but it's functional.

--- src/main/java/ml/introspectsoft/freemium_library/FreemiumHelper.kt
+++ src/main/java/ml/introspectsoft/freemium_library/FreemiumHelper.kt
@@ -4,105 +4,197 @@
 
 package ml.introspectsoft.freemium_library
 
-import android.content.Context
-import com.android.billingclient.api.*
+import android.app.Activity
+import android.content.Context.MODE_PRIVATE
+import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.Purchase
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.disposables.Disposable
+import ml.introspectsoft.rxbilling.BillingResponse
+import ml.introspectsoft.rxbilling.Inventory
+import ml.introspectsoft.rxbilling.PurchasesUpdate
+import ml.introspectsoft.rxbilling.RxBilling
+import rx.subjects.PublishSubject
+import rx.subjects.ReplaySubject
 import timber.log.Timber
+import java.text.SimpleDateFormat
 
-class FreemiumHelper(context: Context, private val premiumSku: String) :
-    PurchasesUpdatedListener,
-    SkuDetailsResponseListener {
-    private var billingClient: BillingClient =
-        BillingClient.newBuilder(context).setListener(this).enablePendingPurchases().build()
-    private var premiumSkuDetails: SkuDetails? = null
+class FreemiumHelper(private val activity: Activity, private val premiumSku: String) {
+    companion object {
+        const val INVALID_TOKEN = "INVALID"
+        const val PREFERENCE_KEY = "freemium_purchase_token"
+        const val PREFERENCE_FILE = "freemium_purchase_preferences"
+    }
 
-    val isReady get() = billingClient.isReady
+    private var preferenceToken: String = "invalid"
 
-    // Default to true
-    var isAvailable = true
+    private var purchaseSubscription: Disposable? = null
+    private var billing = RxBilling(activity)
+    private var premiumInventory: Inventory? = null
+    private var _isPremium = false
 
-    // From our check if it's a premium version or not. Just saying no for now until we get the rest working.
-    val isPremium get() = true
+    /**
+     * Notification of change to isPremium. Only triggered when it becomes true.
+     */
+    val premiumChanged: @NonNull PublishSubject<Boolean> = PublishSubject.create<Boolean>()
+
+    // TODO: Switch this to a get() only
+    var isAvailable = false
+    val isPremium get() = _isPremium
+
+    /**
+     * onNewPurchase(purchase)
+     * TODO: See if ReplaySubject is going to work
+     */
+    val newPurchase: @NonNull ReplaySubject<Purchase> = ReplaySubject.create<Purchase>()
+
+    /**
+     * onPendingPurchase(purchase)
+     * TODO: See if ReplaySubject is going to work
+     */
+    val pendingPurchase: @NonNull ReplaySubject<Purchase> = ReplaySubject.create<Purchase>()
 
     init {
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
+        // get cached values
+        loadPreference()
+
+        // Start listening for purchase changes
+        purchaseSubscription =
+                billing.purchasesUpdated.subscribe { updates -> onPurchasesUpdated(updates) }
+
+        billing.queryInAppPurchases(premiumSku).subscribe {
+            if (it?.sku == premiumSku) {
+                premiumInventory = it
+                isAvailable = true
+                Timber.d("premiumInventory saved")
+            }
+
+            checkPurchased()
+
+            // Query for existing purchases for INAPP
+            billing.checkPurchases(BillingClient.SkuType.INAPP)
         }
+
     }
 
-    private fun connect(callback: (BillingResult?) -> Unit) {
-        if (!billingClient.isReady) {
-            billingClient.startConnection(
-                BillingClientRequest(
-                    callback
-                )
+    private fun onPurchasesUpdated(update: PurchasesUpdate) {
+        if (update.result.responseCode != BillingResponse.OK) {
+            Timber.d(
+                    "Something probably went wrong. ResponseCode: %d, Purchases count: %d",
+                    update.result.responseCode,
+                    update.purchases?.count()
             )
-        } else {
-            callback(null)
+            return
         }
-    }
 
-    fun querySkuDetails() = connect {
-        setStates(it)
-        val skuList = listOf(premiumSku)
-
-        val params = SkuDetailsParams.newBuilder()
-        params.setSkusList(skuList).setType(BillingClient.SkuType.INAPP)
-        billingClient.querySkuDetailsAsync(params.build(), this)
-    }
-/*
-    fun purchasePro(activity: Activity) {
-        //connect()
-        // Retrieve a value for "skuDetails" by calling querySkuDetailsAsync().
-        val flowParams = BillingFlowParams.newBuilder()
-            .setSkuDetails(premiumSkuDetails)
-            .build()
-        //val responseCode = billingClient.launchBillingFlow(activity, flowParams)
-    }
-*/
-    private fun setStates(billingResult: BillingResult?) {
-        when (billingResult?.responseCode) {
-            BillingClient.BillingResponseCode.OK -> {
-                Timber.v("Billing connected")
-            }
-            BillingClient.BillingResponseCode.BILLING_UNAVAILABLE -> {
-                Timber.v("Billing unavailable on this device.")
-                // Play services are unavailable on the device
-                isAvailable = false
-            }
-            BillingClient.BillingResponseCode.SERVICE_UNAVAILABLE -> {
-                Timber.w("Billing service unavailable")
-            }
-            BillingClient.BillingResponseCode.SERVICE_TIMEOUT -> {
-                Timber.w("Billing service timeout")
-            }
-            BillingClient.BillingResponseCode.SERVICE_DISCONNECTED -> {
-                Timber.v("Billing service disconnected")
-            }
-            else -> {
-                Timber.e(
-                    "Billing connection failed. Unknown response: %s",
-                    (billingResult?.responseCode).toString()
-                )
-            }
-        }
-    }
-
-    override fun onPurchasesUpdated(result: BillingResult?, data: MutableList<Purchase>?) {
-        Timber.v("onPurchasesUpdated:$result:$data")
-    }
-
-    override fun onSkuDetailsResponse(result: BillingResult?, data: MutableList<SkuDetails>?) {
-        if (result?.responseCode == BillingClient.BillingResponseCode.OK && data != null) {
-            for (skuDetails in data) {
-                if (premiumSku == skuDetails.sku) {
-                    premiumSkuDetails = skuDetails
+        update.purchases?.forEach { purchase ->
+            if (!purchase.isAcknowledged) {
+                if (purchase.purchaseState == Purchase.PurchaseState.PURCHASED) {
+                    // handle premium sku ourselves
+                    if (purchase.sku == premiumSku) {
+                        billing.acknowledgePurchase(purchase).subscribe({
+                                                                            if (it != BillingResponse.OK) {
+                                                                                Timber.d(
+                                                                                        "Acknowledge response code: %d",
+                                                                                        it
+                                                                                )
+                                                                            } else {
+                                                                                Timber.d(
+                                                                                        "Purchase (%s) acknowledged",
+                                                                                        purchase.purchaseToken
+                                                                                )
+                                                                                makePremium(purchase.purchaseToken)
+                                                                            }
+                                                                        }, {
+                                                                            Timber.w(it)
+                                                                        })
+                    } else {
+                        // Process any other new purchase in the app
+                        newPurchase.onNext(purchase)
+                    }
+                }
+                if (purchase.purchaseState == Purchase.PurchaseState.PENDING) {
+                    // Informational pending purchase notification
+                    pendingPurchase.onNext(purchase)
+                }
+            } else {
+                // possibly redundant checks, but...
+                if (purchase.sku == premiumSku && purchase.purchaseState == Purchase.PurchaseState.PURCHASED && purchase.isAcknowledged) {
+                    makePremium(purchase.purchaseToken)
                 }
             }
-            Timber.d("SKU data result: $data")
-        } else {
-            Timber.d("onSkuDetailsResponse fail. responseCode: ${result?.responseCode}")
+        }
+    }
+
+    private fun makePremium(tokenString: String) {
+        savePreference(tokenString)
+        _isPremium = true
+        premiumChanged.onNext(_isPremium)
+    }
+
+    private fun checkPurchased() {
+        billing.purchasedInApps.subscribe({
+                                              if (it.purchaseState == Purchase.PurchaseState.PURCHASED && it.productId == premiumSku) {
+                                                  // TODO Add some validation to verify the user really purchased it somehow
+                                                  // TODO: Make this work?
+                                                  Timber.d("need to set isPremium to true")
+                                                  if (preferenceToken != it.purchaseToken || !_isPremium) {
+                                                      makePremium(it.purchaseToken)
+                                                  }
+                                              } else {
+                                                  Timber.d("not showing as purchased")
+                                                  Timber.d("packageName: %s", it.packageName)
+                                                  Timber.d("productId: %s", it.productId)
+                                                  Timber.d("purchaseToken: %s", it.purchaseToken)
+                                                  Timber.d(
+                                                          "purchaseTime: %s",
+                                                          SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(
+                                                                  it.purchaseTime
+                                                          )
+                                                  )
+                                                  Timber.d("purchaseState: %d", it.purchaseState)
+                                              }
+
+                                          }, {
+                                              // Something went wrong
+                                              Timber.w(it)
+                                          })
+    }
+
+    fun purchasePro() {
+        if (premiumInventory == null) {
+            Timber.w("Cannot initiate purchase yet.")
         }
 
-        Timber.d("Sku result: $premiumSkuDetails")
+        premiumInventory?.let { it ->
+            billing.purchase(it).subscribe({ result ->
+                                               Timber.d("Purchase result: %d", result.responseCode)
+                                           }, { t ->
+                                               Timber.e(t)
+                                           })
+        }
+    }
+
+    fun destroy() {
+        if (purchaseSubscription != null && purchaseSubscription?.isDisposed == false) {
+            purchaseSubscription?.dispose()
+        }
+        billing.destroy()
+    }
+
+    private fun loadPreference() {
+        val prefs = activity.applicationContext.getSharedPreferences(PREFERENCE_FILE, MODE_PRIVATE)
+        preferenceToken = prefs.getString(PREFERENCE_KEY, INVALID_TOKEN) ?: INVALID_TOKEN
+
+        if (preferenceToken != INVALID_TOKEN) {
+            // Will run a verify later.
+            _isPremium = true
+        }
+    }
+
+    private fun savePreference(purchaseToken: String) {
+        preferenceToken = purchaseToken
+        val prefs = activity.applicationContext.getSharedPreferences(PREFERENCE_FILE, MODE_PRIVATE)
+        prefs.edit().putString(PREFERENCE_KEY, purchaseToken).apply()
     }
 }
